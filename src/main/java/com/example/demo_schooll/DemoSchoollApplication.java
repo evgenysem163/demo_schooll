70882b4049dc6d2d9253f71045cdd6254ca8a6de
evgeny_sem163 <evg82tol@gmail.com>
2023-06-25 19:03:52 +0300

    часть 1

---
+++ src/main/java/com/example/demo_schooll/DemoSchoollApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_schooll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSchoollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSchoollApplication.class, args);
+	}
+
+}
